9b771e001b67c44216517a3c610c9f38f7b9e9fd
MJNabor <mariajhazmine.nabor@gmail.com>
2021-07-06 17:40:54 +0800

    Test Case 2

--- discountProgram.c
+++ discountProgram.c
@@ -4,8 +4,10 @@ int main() {
     
     int book[2] = {0, 0};
     
-    printf("Enter the number of Book sold: ");
+    printf("Enter the number of Book 1 sold: ");
     scanf("%d", &book[0]);
+    printf("Enter the number of Book 2 sold: ");
+    scanf("%d", &book[1]);
     
     int totalNumberOfBooks;
     totalNumberOfBooks = book[0] + book[1];
@@ -17,13 +19,13 @@ int main() {
     int bookInSeries = 0;
     float discountedPrice = 0;
     
-    if (book[0] != 0)
+    if (book[0] != 0 && book[1] != 0)
     {
-        totalBookPrice = bookPrice;
-        discountedPrice = totalBookPrice;
+        totalBookPrice = bookPrice * 2;
+        discountedPrice = totalBookPrice-(totalBookPrice*discount);
     }
-
-    printf("totalBookPrice is %f \n", totalBookPrice);
+    printf("totalBookPrice with 2 books in series %f \n", totalBookPrice);
+    printf("discounted price is %f \n", discountedPrice);
     
     return 0;
 }
