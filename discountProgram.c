b8d4e79d45d1e889c006935bc07f3274844a5ac0
MJNabor <mariajhazmine.nabor@gmail.com>
2021-07-06 17:43:47 +0800

    Test Case 6

--- discountProgram.c
+++ discountProgram.c
@@ -28,7 +28,7 @@ int main() {
     float discount = 0.05;
     float discountedPrice = 0;
     
-    if (book[0] != 0 && book[1] != 0 && book[2] == 0 && book[3] == 0 && book[4] == 0)
+/*    if (book[0] != 0 && book[1] != 0 && book[2] == 0 && book[3] == 0 && book[4] == 0)
     {
         totalBookPrice = bookPrice * 2;
         discountedPrice = totalBookPrice-(totalBookPrice*discount);
@@ -50,8 +50,52 @@ int main() {
         totalBookPrice = bookPrice * 5;
         discount = discount*5;
         discountedPrice = totalBookPrice-(totalBookPrice*discount);
+    }*/
+    
+    int seriesBought = 0;
+    int totalNumberOfBooksCounter = totalNumberOfBooks;
+    
+    while (totalNumberOfBooksCounter != 0)
+    {
+        int count;
+        float totalBookPriceTemp;
+        float discountTemp;
+        float discountedPriceTemp;
+        
+        for (count = 0; count < bookInSeries; count++)
+        {
+            if (book[count] != 0)
+            {
+                book[count]--;
+                seriesBought++;
+                totalNumberOfBooksCounter--;
+            }
+        }
+        
+        printf("seriesBought is %d \n", seriesBought);
+        
+        if (seriesBought != 0)
+        {
+            totalBookPriceTemp = bookPrice * seriesBought;
+            if (seriesBought != 3)
+            {
+                discountTemp = discount * seriesBought;
+            }
+            else
+            {
+                discountTemp = discount * 2;
+            }
+            
+            printf("discountTemp is %f \n", discountTemp);
+            discountedPriceTemp = totalBookPriceTemp - (totalBookPriceTemp * discountTemp);
+            
+            seriesBought = 0;
+        }
+        
+        totalBookPrice = totalBookPrice + totalBookPriceTemp;
+        discountedPrice = discountedPrice + discountedPriceTemp;
     }
-
+    
     printf("totalBookPrice with %d books in series %f \n", totalNumberOfBooks, totalBookPrice);
     printf("discounted price is %f \n", discountedPrice);
     
