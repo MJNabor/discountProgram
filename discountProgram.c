e77921638b0076da91fa7f3e9885c80571523d66
MJNabor <mariajhazmine.nabor@gmail.com>
2021-07-06 17:41:40 +0800

    Test Case 3

--- discountProgram.c
+++ discountProgram.c
@@ -2,29 +2,42 @@ int main() {
     
     float bookPrice = 8;
     
-    int book[2] = {0, 0};
+    int bookInSeries = 3;
+    
+    int book[3] = {0, 0, 0};
     
     printf("Enter the number of Book 1 sold: ");
     scanf("%d", &book[0]);
     printf("Enter the number of Book 2 sold: ");
     scanf("%d", &book[1]);
+        printf("Enter the number of Book 3 sold: ");
+    scanf("%d", &book[2]);
     
-    int totalNumberOfBooks;
-    totalNumberOfBooks = book[0] + book[1];
+    int totalNumberOfBooks = 0;
+    for (int count = 0; count < bookInSeries; count++)
+    {
+        totalNumberOfBooks = totalNumberOfBooks + book[count];
+    }
     printf("Total number of books is %d \n", totalNumberOfBooks);
     
     float totalBookPrice = 0;
     float discount = 0.05;
     
-    int bookInSeries = 0;
     float discountedPrice = 0;
     
-    if (book[0] != 0 && book[1] != 0)
+    if (book[0] != 0 && book[1] != 0 && book[2] == 0)
     {
         totalBookPrice = bookPrice * 2;
         discountedPrice = totalBookPrice-(totalBookPrice*discount);
     }
-    printf("totalBookPrice with 2 books in series %f \n", totalBookPrice);
+    else if (book[0] != 0 && book[1] != 0 && book[2] != 0)
+    {
+        totalBookPrice = bookPrice * 3;
+        discount = discount*2;
+        discountedPrice = totalBookPrice-(totalBookPrice*discount);
+    }
+
+    printf("totalBookPrice with %d books in series %f \n", totalNumberOfBooks, totalBookPrice);
     printf("discounted price is %f \n", discountedPrice);
     
     return 0;
